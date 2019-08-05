a652e15f55eb91932c2464ef517d65085e17e694
riosun269 <53744710+riosun269@users.noreply.github.com>
2019-08-05 15:17:34 +0700

    Add files via upload

--- PG3.java
+++ PG3.java
@@ -1,9 +1,35 @@
 package newpackage;
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.Select;
 
 public class PG3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		String baseUrString = "http://demo.guru99.com/test/newtours/register.php";
+		driver.get(baseUrString);
+		
+		WebElement dropbox = driver.findElement(By.xpath("\\select[@name='country']"));
+		dropbox.selectBy
+	
+		
+		
+		
+		driver.close();
+	
 
 	}
 
